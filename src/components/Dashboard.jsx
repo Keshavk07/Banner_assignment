da238bea3c4afaeacfc508724ab2f62bac074078
Keshavk07 <keshavk289@gmail.com>
2024-08-15 11:17:53 +0530

    commit

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -10,7 +10,7 @@ const Dashboard = ({ onUpdate }) => {
   useEffect(() => {
     const fetchBanner = async () => {
       try {
-        const response = await axios.get('http://localhost:2033/api/banner');
+        const response = await axios.get('https://banner-assignment-grl5.onrender.com/api/banner');
         setDescription(response.data.description);
         setTimer(response.data.timer);
         setLink(response.data.link);
@@ -25,7 +25,7 @@ const Dashboard = ({ onUpdate }) => {
 
   const handleUpdate = async () => {
     try {
-      await axios.post('http://localhost:2033/api/banner', { description, timer, link });
+      await axios.post('https://banner-assignment-grl5.onrender.com/api/banner', { description, timer, link });
       if (onUpdate) {
         onUpdate({ description, timer, link, visible: bannerVisible });
       }
